ae7e8ab9fc1fcd3ebc3b66133c19e69a2444d7cd
molepi <m.van_iterson@lumc.nl>
2017-03-24 08:52:24 +0100

    adding Zhou/Hop annotation

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' title
+#'
+#' description
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{maye Zhou here?}
+"hg19.GoNLsnps"
+
+
+#' title
+#'
+#' description
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{}
+"hm450.manifest.pop.GoNL"
