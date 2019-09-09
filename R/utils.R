7a3a14b4765af01e167a6c4b21f1f0d9c4a967d8
ljsinke <48981890+ljsinke@users.noreply.github.com>
2019-09-09 12:46:51 +0200

    Update utils.R
    Further fix to read.metharray.exp.par()

--- R/utils.R
+++ R/utils.R
@@ -18,7 +18,7 @@ read.metharray.exp.par <- function(targets, verbose = TRUE, ...) {
     if (nworkers <= 1)
         stop("Did you registered a biocparallel back-end?")
     y <- rep(1, ceiling(nrow(targets)/nworkers))
-    for (i in 2:workers) y <- c(y, rep(i, ceiling(nrow(targets)/nworkers)))
+    for (i in 2:nworkers) y <- c(y, rep(i, ceiling(nrow(targets)/nworkers)))
     y <- y[1:nrow(targets)]
     jobs <- split(targets, y)
 
